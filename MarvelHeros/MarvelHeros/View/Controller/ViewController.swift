7099abe6332855ee9813c5c14aed92549cf88be0
Rodrigo Pacheco <rodrigo.rp.pacheco@gmail.com>
2018-12-23 10:05:51 -0300

    MVVM structure

---
+++ MarvelHeros/MarvelHeros/View/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelHeros
+//
+//  Created by Rodrigo Pacheco on 23/12/18.
+//  Copyright © 2018 RodrigoPacheco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
